b2e4a836735ddc9724e13dbf354c1e5cb9f746ed
Your Name <nick@nickcohen.com>
2011-08-10 15:27:45 -0500

    triangle puzzle

---
+++ triangle/TrianglePuzzle.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import com.sif.reverse.ReverseFileLineReader;
+
+public class TrianglePuzzle {
+	private ReverseFileLineReader in;
+	public TrianglePuzzle(String path) throws IOException {
+		File f = new File(path);
+		in = new ReverseFileLineReader(new File(path));
+	}
+	
+	public int solve() throws IOException {
+		String line;
+		int [] sums = null;
+		StringTokenizer st;
+		while((line = in.readLine()) != null) {
+			st = new StringTokenizer(line);
+			int numTokens = st.countTokens();
+			if(sums == null) {
+				// allocate first time, if memory is an issue we can increase complexity and cut memory usage in half
+				sums = new int[numTokens];
+				int j = 0;
+				while(st.hasMoreTokens()) {
+					sums[j] = Integer.parseInt(st.nextToken());
+					j++;
+				}
+				continue;
+			}
+			int j = 0;
+			while(st.hasMoreTokens()) {
+				int current = Integer.parseInt(st.nextToken());
+				// previous interation sums are at j and j+1
+				int index = j;
+				int index_plus_one = index+1;
+				if(sums[index] > sums[index_plus_one]) {
+					sums[j] = current + sums[index];
+				}
+				else {
+					sums[j] = current + sums[index_plus_one];
+				}
+				j++;
+			}
+		}
+		return sums[0];
+	}
+	
+	public int solveBinaryTreeProblem() throws IOException {
+		int c;
+		String line;
+		int count = 0;
+		int [] sums = null;
+		StringTokenizer st;
+		while((line = in.readLine()) != null) {
+			st = new StringTokenizer(line);
+			int numTokens = st.countTokens();
+			if(sums == null) {
+				// allocate first time
+				sums = new int[numTokens];
+				int j = 0;
+				while(st.hasMoreTokens()) {
+					sums[j] = Integer.parseInt(st.nextToken());
+					j++;
+				}
+				continue;
+			}
+			int j = 0;
+			while(st.hasMoreTokens()) {
+				int current = Integer.parseInt(st.nextToken());
+				// previous interation sums are at j*2 and j*2+1
+				int index = j << 1;
+				int index_plus_one = index+1;
+				if(sums[index] > sums[index_plus_one]) {
+					sums[j] = current + sums[index];
+				}
+				else {
+					sums[j] = current + sums[index_plus_one];
+				}
+			}
+		}
+		return sums[0];
+	}
+
+	public static void main(String[] args) throws Exception{
+		TrianglePuzzle p = new TrianglePuzzle(args[0]);
+		int solution = p.solve();
+		System.out.println(solution);
+	}
+}
